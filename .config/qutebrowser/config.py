6adc1d54ef4e8fe893eeea57d3b22bcdb4b2e3c5
Anders Mårtensson <anders@nemrod.se>
2018-01-23 22:54:24 +0100

    add qutebrowser conf

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,12 @@
+config.load_autoconfig()
+
+c.tabs.background = True
+
+config.bind(';D', 'hint links spawn transmission-remote -a {hint-url}')
+config.bind(';P', 'hint links spawn mpv {hint-url}')
+config.bind(',p', 'spawn mpv {url}')
+
+c.url.searchengines['anime'] = 'https://myanimelist.net/search/all?q={}'
+c.url.searchengines['ja'] = 'http://tangorin.com/general/{}'
+c.url.searchengines['manga'] = 'http://www.mangafox.com/search.php?name={}'
+c.url.searchengines['w'] = 'https://en.wikipedia.org/w/index.php?search={}'

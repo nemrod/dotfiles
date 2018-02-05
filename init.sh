c15a88dd55244b08d5905235cb5b6d3978d0aed5
Anders Mårtensson <anders@nemrod.se>
2018-02-05 23:11:07 +0100

    remove urxvt

--- init.sh
+++ init.sh
@@ -3,11 +3,6 @@ if ! [ -d ~/.zplug ]; then
 	curl -sL zplug.sh/installer | zsh
 fi
 
-# URxvt
-if ! [ -d ~/.urxvt/ext ]; then
-	curl -fLo ~/.urxvt/ext/font-size --create-dirs https://raw.githubusercontent.com/majutsushi/urxvt-font-size/master/font-size
-fi
-
 # Base16
 if ! [ -d ~/.config/base16-shell ]; then
 	git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell

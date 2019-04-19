e4495718e5e22ad337451f83c7af29c91419f3a5
Anders Mårtensson <anders@nemrod.se>
2019-04-19 21:40:07 +0200

    update qb search engines

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -6,8 +6,9 @@ config.bind(';D', 'hint links spawn transmission-remote -a {hint-url}')
 config.bind(';P', 'hint links spawn mpv {hint-url}')
 config.bind(',p', 'spawn mpv {url}')
 
-c.url.searchengines['anime'] = 'https://myanimelist.net/search/all?q={}'
-c.url.searchengines['ja'] = 'http://tangorin.com/general/{}'
-c.url.searchengines['manga'] = 'http://www.mangahere.cc/search.php?name={}'
+c.url.searchengines['anime'] = 'https://myanimelist.net/anime.php?q={}'
+c.url.searchengines['ja'] = 'https://tangorin.com/words?search={}'
+c.url.searchengines['manga'] = 'https://mangadex.org/search?title={}'
 c.url.searchengines['w'] = 'https://en.wikipedia.org/w/index.php?search={}'
 c.url.searchengines['yt'] = 'https://www.youtube.com/results?search_query={}'
+c.url.searchengines['maps'] = 'https://www.google.com/maps/?q={}'

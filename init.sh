fd61ac1ec05ef59f7fe154ca327e7199ca276a0b
Anders Mårtensson <anders@nemrod.se>
2019-05-01 13:43:30 +0200

    update zplug installation

--- init.sh
+++ init.sh
@@ -1,9 +1,7 @@
-# zsh
 if ! [ -d ~/.zplug ]; then
-	curl -sL zplug.sh/installer | zsh
+	curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 fi
 
-# Base16
 if ! [ -d ~/.config/base16-shell ]; then
 	git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 	zsh -ic base16_twilight

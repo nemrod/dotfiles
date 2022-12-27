da7d35162ffd485456091644cf72e922f5b5481a
Anders Mårtensson <anders@nemrod.se>
2022-12-27 13:04:16 +0100

    update qute search engines

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -15,9 +15,15 @@ config.bind(',cn', 'spawn catt cast {url}')
 config.bind(',cq', 'spawn catt add {url}')
 
 c.url.searchengines['anime'] = 'https://myanimelist.net/anime.php?q={}'
-c.url.searchengines['ja'] = 'https://jisho.org/search/{}' #https://tangorin.com/words?search={}'
-c.url.searchengines['manga'] = 'https://mangadex.org/search?title={}'
+c.url.searchengines['ja'] = 'https://jisho.org/search/{}'
+c.url.searchengines['manga'] = 'https://mangadex.org/titles?page=1&q={}&translatedLang=en,sv&order=relevance.desc'
 c.url.searchengines['w'] = 'https://en.wikipedia.org/w/index.php?search={}'
+c.url.searchengines['sw'] = 'https://sv.wikipedia.org/w/index.php?search={}'
 c.url.searchengines['yt'] = 'https://www.youtube.com/results?search_query={}'
 c.url.searchengines['maps'] = 'https://www.google.com/maps/?q={}'
 c.url.searchengines['tradera'] = 'https://www.tradera.com/search?q={}&queryScope=AnyWordsAnyOrder&searchInLongDescription=true&sortBy=AddedOn'
+c.url.searchengines['tr'] = 'https://translate.google.com/?sl=auto&tl=en&text={}&op=translate'
+
+for mode in ["normal", "caret"]:
+    config.bind(',t', 'spawn --userscript yomichad', mode=mode)
+    config.bind(',T', 'spawn --userscript yomichad --prefix-search', mode=mode)
